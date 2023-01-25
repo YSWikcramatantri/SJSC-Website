e0baede8bcdaa7450be65c7ba34660888e0bdc48
YSWickramatantri <yowun2008@gmail.com>
2023-01-25 21:43:20 +0530

    JS FIle

---
+++ main.js
@@ -0,0 +1,18 @@
+var myvar;
+
+function timeOut() {
+    myvar = setTimeout(preloader, 3000);
+}
+
+function preloader() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("page-content").style.display = "block";
+}
+
+
+function redierctSinhala() {
+    var checkbox = document.getElementById("language-switch");
+    if (checkbox.checked == true) {
+        location.replace("SIVALI JUNIOR SCIENCE CLUB WEBSITE [SJSCW].html")
+    }
+}
